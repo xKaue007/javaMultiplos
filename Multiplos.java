2786869b6c280d80dc0985a92ee1853ae31b4cf2
xKaue007 <kaueoliveira92@gmail.com>
2021-08-22 20:01:27 +0000

    Multiplos

---
+++ Multiplos.java
@@ -0,0 +1,8 @@
+public class Multiplos{
+
+    public static void main (String args[]){
+        for(int i=1; i<=100; i=i*3){
+            System.out.println("Os Multiplos são: "+i);
+        }
+    }
+}
